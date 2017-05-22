b17b6e026134bedfdda777fabf21bf3f6d6db2eb
Ben <bwyap@outlook.com>
2017-05-23 03:03:10 +1000

    Remove unecessary hashmap

--- src/bwyap/familyfeud/gui/UIManager.java
+++ src/bwyap/familyfeud/gui/UIManager.java
@@ -18,9 +18,7 @@ public class UIManager {
 		MACOS;
 	}
 	
-	private static UIManager manager;
-	
-	private Map<SupportedOS, Map<String, Integer>> dimensions;
+	private static UIManager INSTANCE;
 	
 	private Map<String, Integer> currentOSDimensions;
 	
@@ -29,18 +27,21 @@ public class UIManager {
 	 * @return
 	 */
 	public static UIManager getInstance() {
-		if (manager == null) {
-			manager = new UIManager();
+		if (INSTANCE == null) {
+			INSTANCE = new UIManager();
 		}
-		return manager;
+		return INSTANCE;
 	}
 	
 	private UIManager() {
-		dimensions = new HashMap<SupportedOS, Map<String, Integer>>();
-		loadMacOSDimensions();
-		loadWindowsDimensions();
+		// Load dimensions according to detected UI
+		if (OSValidator.isMac()) loadMacOSDimensions();
+		else if (OSValidator.isWindows()) loadWindowsDimensions();
 	}
 	
+	/**
+	 * Load macOS dimensions
+	 */
 	private void loadMacOSDimensions() {
 		// TODO make this more dynamic
 		Map<String, Integer> map = new HashMap<String, Integer>();
@@ -78,16 +79,50 @@ public class UIManager {
 		map.put("gamewindow.width", 1024);
 		map.put("gamewindow.height", 768);
 		
-		dimensions.put(SupportedOS.MACOS, map);
+		currentOSDimensions = map;
 	}
 	
+	/**
+	 * Load Windows dimensions
+	 */
 	private void loadWindowsDimensions() {
 		// TODO make this more dynamic
 		Map<String, Integer> map = new HashMap<String, Integer>();
 		
+		map.put("controlwindow.width", 800);
+		map.put("controlwindow.height", 640);
+		  
+		map.put("windowcontrolpanel.width", 180);
+		map.put("windowcontrolpanel.height", 120);
 		
+		map.put("statepanel.width", 180);
+		map.put("statepanel.height", 220);
+		  
+		map.put("consolepanel.width", map.get("controlwindow.width") - 10);
+		map.put("consolepanel.height", 200);
+		  
+		map.put("stateplaypanel.width", map.get("statepanel.width"));
+		map.put("stateplaypanel.height", map.get("controlwindow.height") - map.get("consolepanel.height") - map.get("statepanel.height") - 25);
+		  
+		map.put("familypanel.width", map.get("windowcontrolpanel.width"));
+		map.put("familypanel.height", map.get("stateplaypanel.height"));
 		
-		dimensions.put(SupportedOS.WINDOWS, map);
+		map.put("managefamilypanel.width", map.get("statepanel.width"));
+		map.put("managefamilypanel.height", map.get("statepanel.height") - map.get("windowcontrolpanel.height"));
+		  
+		map.put("questionselectionpanel.width", map.get("controlwindow.width") - map.get("statepanel.width") - map.get("windowcontrolpanel.width") - 10);
+		map.put("questionselectionpanel.height", (int)(0.8 * map.get("statepanel.height")));
+		  
+		map.put("questionsetloaderpanel.width", map.get("controlwindow.width") - map.get("statepanel.width") - map.get("windowcontrolpanel.width") - 10);
+		map.put("questionsetloaderpanel.height", map.get("statepanel.height") - map.get("questionselectionpanel.height"));
+
+		map.put("questioncontrolpanel.width", map.get("questionselectionpanel.width"));
+		map.put("questioncontrolpanel.height", map.get("stateplaypanel.height"));
+		
+		map.put("gamewindow.width", 1024);
+		map.put("gamewindow.height", 768);
+		
+		currentOSDimensions = map;
 	}
 	
 	/**
@@ -96,15 +131,6 @@ public class UIManager {
 	 * @return
 	 */
 	public int getDimension(String propertyname) {
-		if (currentOSDimensions == null) {
-			if (OSValidator.isMac()) {
-				currentOSDimensions = dimensions.get(SupportedOS.MACOS);
-			}
-			else if (OSValidator.isWindows()) {
-				currentOSDimensions = dimensions.get(SupportedOS.WINDOWS);
-			}
-		}
-		
 		return currentOSDimensions.get(propertyname);
 	}
 	
